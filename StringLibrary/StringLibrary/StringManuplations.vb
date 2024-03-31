07b588a253cc27976af2bf81660ec485a762a01c
Ayush Patel <patelayush1929@gmail.com>
2024-03-31 16:41:30 -0400

    Add project files.

---
+++ StringLibrary/StringLibrary/StringManuplations.vb
@@ -0,0 +1,12 @@
+﻿Public Class StringManipulations
+    Public Shared Function ReverseString(input As String) As String
+        Dim charArray As Char() = input.ToCharArray()
+        Array.Reverse(charArray)
+        Return New String(charArray)
+    End Function
+
+    Public Shared Function ConcatenateStrings(ByVal ParamArray strings() As String) As String
+        Return String.Join("", strings)
+    End Function
+End Class
+
---
+++ StringManipulationApp/StringManipulationApp/Module1.vb
@@ -0,0 +1,18 @@
+﻿Imports StringLibrary
+
+Module Module1
+
+    Sub Main()
+        Dim originalString As String = "Hello, World!"
+        Dim reversedString As String = StringManipulations.ReverseString(originalString)
+        Dim concatenatedString As String = StringManipulations.ConcatenateStrings("Hello", ", ", "World", "!")
+
+        Console.WriteLine($"Original: {originalString}")
+        Console.WriteLine($"Reversed: {reversedString}")
+        Console.WriteLine($"Concatenated: {concatenatedString}")
+
+        Console.ReadKey()
+    End Sub
+
+
+End Module
